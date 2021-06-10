c21110f9bd6ec9da574ed3b9b98f1b0a9063f8fb
Ash Schofield <ashley.schofield83@gmail.com>
2021-06-10 16:27:06 +1000

    cleaned up nvme echos

--- scripts/nvme.sh
+++ scripts/nvme.sh
@@ -3,9 +3,12 @@ DEV=/dev/nvme0n1
 PART=/dev/nvme0n1p1
 FOLDER=/var/lib/openalpr
 
+echo "Do we have $DEV connected ?"
 if [ -e $DEV ]; then
 	echo "$DEV is connected"
-	if [ ! -e $PART ]; then
+	
+    echo "Do we have $PART created ?"
+    if [ ! -e $PART ]; then
 		echo "Creating partition"
 		sudo parted $DEV mklabel gpt 
 		sudo parted $DEV mkpart primary ext4 0 100%
@@ -14,6 +17,7 @@ if [ -e $DEV ]; then
 		echo "$PART already exists"
 	fi
 	
+    echo "Do we have $FOLDER created ?"
 	if [ ! -d $FOLDER ]; then
 		echo "Creating $FOLDER"
 		sudo mkdir $FOLDER;
@@ -21,11 +25,15 @@ if [ -e $DEV ]; then
 		echo "$FOLDER already exists"
 	fi
 	
+    echo "Lets try mount $FOLDER to $PART"
 	if ([ -e $PART ] && [ -d $FOLDER ]); then
-		echo "mounting $FOLDER to $PART"
+		echo "Mounting $FOLDER to $PART"
 		sudo mount $PART $FOLDER
+    else
+        echo "Problem occured with either $FOLDER or $PART"
 	fi
 	
+    echo "Adding the auto mount of $FOLDER to $PART in FSTAB"
 	if ! grep -q '#2TB' /etc/fstab; then
 		echo "Making backup of fstab"
 		sudo cp /etc/fstab /etc/fstab.backup
@@ -33,7 +41,7 @@ if [ -e $DEV ]; then
 		echo '#2TB' >> /etc/fstab
 		echo "$PART    $FOLDER    ext4    defaults    0    2" >> /etc/fstab
 	else
-		echo "#2TB already in fstab"
+		echo "#2TB is already in fstab"
 	fi
 else
 	echo "$DEV is NOT connected !"
