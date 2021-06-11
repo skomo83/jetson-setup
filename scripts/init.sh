7df05bfc69d722da2f538528e84435a7599e58d8
Ash Schofield <ashley.schofield83@gmail.com>
2021-06-11 13:01:48 +1000

    using raw.githubusercontent

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #go to the dir you want your scripts and copy and paste the below
-#wget https://github.com/skomo83/jetson-setup/blob/main/scripts/init.sh
+#wget https://raw.githubusercontent.com/skomo83/jetson-setup/main/scripts/init.sh
 
 
-wget https://github.com/skomo83/jetson-setup/blob/main/scripts/setup.sh
+wget https://raw.githubusercontent.com/skomo83/jetson-setup/main/scripts/setup.sh
 chmod +x setup.sh
 
-wget https://github.com/skomo83/jetson-setup/blob/main/scripts/nvme.sh
+wget https://raw.githubusercontent.com/skomo83/jetson-setup/main/scripts/nvme.sh
 chmod +x nvme.sh
 
