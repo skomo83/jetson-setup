e5ee3e6eb1667cf44570a21d9b6fc0b1371fd079
Ash Schofield <ashley.schofield83@gmail.com>
2021-06-14 10:24:28 +1000

    checking if entry in the sudo file

--- scripts/sudoadd.sh
+++ scripts/sudoadd.sh
@@ -27,7 +27,16 @@ if [ -z $USERNAME ]; then
 fi
 
 echo -e "$PURPLE ADD $USERNAME to SUDOERS $END"
-sudo bash -c "echo '$USERNAME ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers.d/99_sudo_include_file"   
+
+SUDOSTRING="$USERNAME ALL=(ALL) NOPASSWD:ALL"
+if ! grep -q "$SUDOSTRING" /etc/sudoers.d/99_sudo_include_file; then
+    echo "$RED not in the file $END"
+    echo "$SUDOSTRING"
+else
+    echo "$GREEN Already in the file $END"
+fi
+
+#sudo bash -c "echo $SUDOSTRING >> /etc/sudoers.d/99_sudo_include_file"   
 
 #Check that your sudoers include file passed the visudo syntax checks:
-sudo visudo -cf /etc/sudoers.d/99_sudo_include_file
+#sudo visudo -cf /etc/sudoers.d/99_sudo_include_file
