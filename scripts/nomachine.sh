498fff2ee433edaa1693f40e5985b19c1b8d413b
Ash Schofield <ashley.schofield83@gmail.com>
2021-06-17 16:14:02 +1000

    check arm version for download

--- scripts/nomachine.sh
+++ scripts/nomachine.sh
@@ -6,6 +6,7 @@ END="\e[0m"
 
 #need to add a check to see if nomachine is already installed
 PACKAGE=nomachine
+ARMV=$(uname -m | grep -o -E '[0-9]+')
 
 echo -e "$PURPLE Checking if $PACKAGE is installed $END"
 
@@ -14,7 +15,7 @@ dpkg -s $PACKAGE &> /dev/null
 if [ $? -ne 0 ]
     then
         echo -e "$RED $PACKAGE is not installed $END" 
-        wget https://www.nomachine.com/free/arm/v8/deb -O nomachine.deb
+        wget "https://www.nomachine.com/free/arm/v$ARMV/deb" -O nomachine.deb
         sudo dpkg -i nomachine.deb
     else
         echo -e "$GREEN $PACKAGE is already installed $END"
