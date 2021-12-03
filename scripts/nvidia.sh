31d6545eaefccacfd7f9ff5f3a70a2970507b0ad
Ash Schofield <ashley.schofield83@gmail.com>
2021-12-03 12:04:08 +1000

    Added device type for Jetson TXT

--- scripts/nvidia.sh
+++ scripts/nvidia.sh
@@ -7,13 +7,14 @@ END="\e[0m"
 #sudo nano /etc/apt/sources.list.d/nvidia-l4t-apt-source.list
 #edit section below so you can update to 32.5.1 jetpack
 #t194 for Jetson AGX Xavier series or Jetson Xavier NX
+#t186 for Jetson
 #t210 is for Jetson TX1 and Nano series
 #deb https://repo.download.nvidia.com/jetson/common r32.6 main
 #deb https://repo.download.nvidia.com/jetson/t194 r32.6 main
 
 NVFILE=/etc/apt/sources.list.d/nvidia-l4t-apt-source.list
 JETPACK=r32.6
-DEVICE=t194
+DEVICE=t186
 
 echo ""
 echo -e "$GREEN Modifiying the nvidia repo for the Jetpack $DEVICE v $JETPACK $END"
