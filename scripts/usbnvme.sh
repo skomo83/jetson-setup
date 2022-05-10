45f933296cbb1be953aba79f46b5944fc57eae02
Ash Schofield <ashley.schofield83@gmail.com>
2022-05-10 12:35:38 +1000

    bug fix

--- scripts/usbnvme.sh
+++ scripts/usbnvme.sh
@@ -7,7 +7,7 @@ END="\e[0m"
 #echo 174c:2362:u | sudo tee /sys/module/usb_storage/parameters/quirks
 	#lets add a usb3 driver override for LINDY43095 USB to NVME adapter
 QUIRKSTRING="174c:2362:u" 
-QUIRKLOC"/sys/module/usb_storage/parameters/quirks"
+QUIRKLOC="/sys/module/usb_storage/parameters/quirks"
 echo -e "$PURPLE Adding the quirk string override of $QUIRKSTRING to $QUIRKLOC $END"
 
 if ! grep -q "$QUIRKSTRING" $QUIRKLOC; then
