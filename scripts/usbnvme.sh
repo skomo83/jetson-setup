925c5b0094a51a1473dfd133f5da8b6817825c78
Ash Schofield <ashley.schofield83@gmail.com>
2022-05-10 16:42:48 +1000

    changed file edit

--- scripts/usbnvme.sh
+++ scripts/usbnvme.sh
@@ -6,14 +6,16 @@ END="\e[0m"
 
 #echo 174c:2362:u | sudo tee /sys/module/usb_storage/parameters/quirks
 	#lets add a usb3 driver override for LINDY43095 USB to NVME adapter
-QUIRKSTRING="174c:2362:u" 
-QUIRKLOC="/sys/module/usb_storage/parameters/quirks"
-echo -e "$PURPLE Adding the quirk string override of $QUIRKSTRING to $QUIRKLOC $END"
+QUIRKSTRING="usb_storage.quirks=0x174c:0x2362:u" 
+QUIRKLOC="/boot/extlinux/extlinux.conf"
+BACKUPLOC="/home/localadmin/Downloads/"
+echo -e "$PURPLE Adding the usb_storage.quirk override of $QUIRKSTRING to $QUIRKLOC $END"
 
 if ! grep -q "$QUIRKSTRING" $QUIRKLOC; then
-	#cannot backup the file
-	echo -e "$GREEN Inserting  $END"
-	echo "$QUIRKSTRING"	| sudo tee $QUIRKLOC
+	echo -e "$GREEN Making backup of extlinux.conf $END"
+	sudo cp $QUIRKLOC $BACKUPLOC
+	#echo -e "$GREEN Inserting  $END"
+	#echo "$QUIRKSTRING"	| sudo tee $QUIRKLOC
 else
 	echo -e "$GREEN $QUIRKSTRING is already in quirk $END"
 fi
