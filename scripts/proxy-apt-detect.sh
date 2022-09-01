4a4575bda68d539299537f029ed9180178a88d7d
Ash Schofield <ashley.schofield83@gmail.com>
2022-09-01 12:56:41 +1000

    shortend script

--- scripts/proxy-apt-detect.sh
+++ scripts/proxy-apt-detect.sh
@@ -1,29 +1,10 @@
 #!/bin/bash
-RED="\e[31m"
-GREEN="\e[32m"
-PURPLE="\e[35m"
-END="\e[0m"
 
-#IP=192.168.88.1
+IP=192.168.179.168
 PORT=3142
 
-HELPMSG="
- Must run with IP address '-i IPADDRESS' and Port '-p 3142' 
-"
-
-((!$#)) && echo -n "DIRECT"
-
-while getopts i:p:h flag
-do
-    case "${flag}" in
-        i) IP=${OPTARG};;
-		p) PORT=${OPTARG};;
-		h) echo -e "$RED $HELPMSG $END"; exit
-    esac
-done
-
-if nc -w1 -z $IP $PORT; then
-    echo -n "http://${IP}:${PORT}"
+if nc -zw1 $IP $PORT; then
+    echo -n "http://$IP:$PORT"
 else
     echo -n "DIRECT"
 fi
